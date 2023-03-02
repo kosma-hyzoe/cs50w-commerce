c9a532cf4219a82cd54594a5a7f3cb0b3478ad6e
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-03-02 12:08:53 +0100

    outline for "create new listing" functionality

---
+++ auctions/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class Listing:
+    pass
+
+
+class Bids:
+    pass
+
+
+class Comment:
+    pass
+
+
